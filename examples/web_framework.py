5713834cb28b6250c8eadac9f9bc31823c4d3c6f
gram <git@orsinium.dev>
2023-10-31 13:40:17 +0100

    improve web framework example

--- examples/web_framework.py
+++ examples/web_framework.py
@@ -1,13 +1,26 @@
+"""A simple web framework on top of uvicorn.
+
+While uvcorn takes care of concurrency, jockey adds a very simple routing,
+simplified endpoint implementation, and type safety. It also gives some unique
+features that you can't find in other web frameworks, like setting priority
+for endpoints.
+"""
 from __future__ import annotations
+
 import asyncio
 from dataclasses import dataclass
 from typing import Callable, Iterator, cast
-import uvicorn
-import jockey
 
+import uvicorn
 from asgiref.typing import (
-    ASGIReceiveCallable, ASGISendCallable, HTTPRequestEvent, HTTPScope, Scope,
+    ASGIReceiveCallable,
+    ASGISendCallable,
+    HTTPRequestEvent,
+    HTTPScope,
+    Scope,
 )
+
+import jockey
 
 Body = bytes        # Payload
 Path = str          # Key
@@ -24,13 +37,13 @@ class Request(jockey.Adapter[Body, Path, Response]):
         yield self.scope['path']
 
     async def get_payload(self) -> Body:
-        body = b""
+        body = b''
         more_body = True
         while more_body:
             message = await self.receive()
             message = cast(HTTPRequestEvent, message)
-            body += message.get("body", b"")
-            more_body = message.get("more_body", False)
+            body += message.get('body', b'')
+            more_body = message.get('more_body', False)
         return body
 
     async def on_success(self, body: Response) -> None:
@@ -47,16 +60,16 @@ class Request(jockey.Adapter[Body, Path, Response]):
 
     async def _send_response(self, status: int, body: Response) -> None:
         await self.send({
-            "type": "http.response.start",  # type: ignore
-            "status": status,
-            "headers": [
-                (b"content-type", b"application/json"),
+            'type': 'http.response.start',  # type: ignore
+            'status': status,
+            'headers': [
+                (b'content-type', b'application/json'),
             ],
         })
         await self.send({
-            "type": "http.response.body",
-            "body": body,
-            "more_body": False,
+            'type': 'http.response.body',
+            'body': body,
+            'more_body': False,
         })
 
 
@@ -100,13 +113,13 @@ class Server:
                 return
 
         # handle requests using executor
-        assert scope["type"] == "http"
+        assert scope['type'] == 'http'
         request = Request(scope, receive, send)
         await self.executor.execute(request)
 
 
 def main() -> None:
-    config = uvicorn.Config(Server().app, interface="asgi3")
+    config = uvicorn.Config(Server().app, interface='asgi3')
     server = uvicorn.Server(config)
     server.run()
 
